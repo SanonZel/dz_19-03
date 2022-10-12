5269bacf73eb4d1713d6949bd850f04536fe2a55
SanonZel <kanatovaha@gmail.com>
2022-10-12 15:55:31 +0600

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var color = ["green", "red", "yellow", "blue", "orange"];
-
-console.log (color);
-
-function colorick() {
-    return console.log(color);
+const color = ["green", "red", "yellow", "blue", "orange", 'фылводфдлыов', 'олфрывлофрыв', ';фдылвфждыв'];
+function colorpick(string, array){
+     for(let i = 0; i < array.length; i++){
+        if(string == array[i]){
+            return console.log(array[i], 'найден');
+        }
+     }
 }
+let str = "orange"
+colorpick(str, color)
 
-color.map(function(item, index){ 
-    console.log(item, index)
-})
- 
-// color.push("magenta", "pink", "violet")  
+// var colorr = ["green", "red", "yellow", "blue", "orange"];
+
+// console.log (color);
+
+// function colorick() {
+//     return console.log(color);
+// }
+
+// color.map(function(item, index){ 
+//     console.log(item, index)
+// })
